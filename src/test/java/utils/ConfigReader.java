6cdd4934c8907ccb6b312adab076653d19abd664
ChatGTHB <cihat.kose@hotmail.com>
2023-11-23 09:25:43 +0100

    Added ConfigReader.java

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    static Properties properties;
+
+    static {
+        try {
+            String filePath = "configuration.properties";
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            System.out.println("File Could'nt Be Found");
+        } catch (IOException e) {
+            System.out.println("File Could'nt be Load From Properties File");
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static String get(String keyName) {
+        return properties.getProperty(keyName);
+    }
+}
