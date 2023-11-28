cb235165dd1bd3c7c4b9f119b761c4bec958252b
ChatGTHB <cihat.kose@hotmail.com>
2023-11-28 14:02:04 +0100

    Added ApiDemosPage.java

---
+++ src/test/java/page/ApiDemosPage.java
@@ -0,0 +1,32 @@
+package page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import utils.Hooks;
+
+public class ApiDemosPage extends Hooks {
+
+    public ApiDemosPage() {
+        PageFactory.initElements(androidDriver, this);
+    }
+
+    public WebElement findelementByAccessibilityId(String id) {
+        return androidDriver.findElementByAccessibilityId(id);
+    }
+
+    public WebElement getAccessibilityButton() {
+        return findelementByAccessibilityId("Access'ibility");
+    }
+
+    public WebElement getViewsButton() {
+        return findelementByAccessibilityId("Views");
+    }
+
+    public WebElement getListsButton() {
+        return findelementByAccessibilityId("Lists");
+    }
+
+    public void clickElement(WebElement element) {
+        element.click();
+    }
+}
