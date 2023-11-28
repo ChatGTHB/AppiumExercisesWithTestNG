ad3538fb58b86174446149f8765f4540ae84fe7d
ChatGTHB <cihat.kose@hotmail.com>
2023-11-28 14:27:20 +0100

    Added HooksWeb.java

---
+++ src/test/java/utils/HooksWeb.java
@@ -0,0 +1,28 @@
+package utils;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HooksWeb {
+    public static AndroidDriver<AndroidElement> androidDriver;
+    static DesiredCapabilities capabilities = new DesiredCapabilities();
+    @BeforeMethod
+    public void setUp() {
+
+        try {
+            capabilities.setCapability("deviceName", ConfigReader.getProperty("deviceName"));
+            capabilities.setCapability("platformName", ConfigReader.getProperty("platformName"));
+            capabilities.setCapability("platformVersion", ConfigReader.getProperty("platformVersion"));
+            capabilities.setCapability("automationName", ConfigReader.getProperty("automationName"));
+            capabilities.setCapability("browserName","safari");
+            androidDriver = new AndroidDriver<>(new URL("http://127.0.0.1:4723"), capabilities);
+        } catch (MalformedURLException exception) {
+            System.err.println("Malformed URL Exception: " + exception.getMessage());
+        }
+    }
+}
